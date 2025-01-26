f101ac366b623321957c0e55ca64cc0e77d4241b
Jake Rieger <contact.jakerieger@gmail.com>
2025-01-25 19:43:54 -0500

    todo genericize vertex data struct for meshes

--- Code/Engine/Mesh.cpp
+++ Code/Engine/Mesh.cpp
@@ -1,13 +1,3 @@
 #include "Mesh.hpp"
 
-namespace x {
-    Mesh::Mesh(Renderer& renderer, const vector<VSInputPosTexNormal>& vertices, const vector<u32>& indices) : _renderer(
-        renderer) {
-        _geometryBuffers.Create(renderer, vertices.data(), vertices.size(), indices.data(), indices.size());
-    }
-
-    void Mesh::Draw() {
-        _geometryBuffers.Bind(_renderer);
-        _renderer.GetContext()->DrawIndexed(_geometryBuffers.GetIndexCount(), 0, 0);
-    }
-}
+namespace x {}
--- Code/Engine/Mesh.hpp
+++ Code/Engine/Mesh.hpp
@@ -10,7 +10,14 @@ namespace x {
         GeometryBuffer<VSInputPosTexNormal> _geometryBuffers{};
 
     public:
-        Mesh(Renderer& renderer, const vector<VSInputPosTexNormal>& vertices, const vector<u32>& indices);
-        void Draw();
+        Mesh(Renderer& renderer, const vector<VSInputPosTexNormal>& vertices, const vector<u32>& indices)
+            : _renderer(renderer) {
+            _geometryBuffers.Create(renderer, vertices.data(), vertices.size(), indices.data(), indices.size());
+        }
+
+        void Draw() {
+            _geometryBuffers.Bind(_renderer);
+            _renderer.GetContext()->DrawIndexed(_geometryBuffers.GetIndexCount(), 0, 0);
+        }
     };
 }
